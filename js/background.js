a0144ee5224e80998114aa1e7f34439bef1e7504
Merten Peetz <dev@morbz.net>
2016-02-23 17:05:15 +0100

    Use event page instead of background page

--- js/background.js
+++ js/background.js
@@ -1,16 +1,23 @@
-/* Remove expired videos */
-// Calculate time
-var expireTime = 1000 * 60 * 60 * 24 * 30; // 1 Month
-var minDate = Date.now() - expireTime;
+// Remove expired videos
+function clearVideos() {
+	// Calculate time
+	var expireTime = 1000 * 60 * 60 * 24 * 30; // 1 Month
+	var minDate = Date.now() - expireTime;
 
-// Iterate videos
-chrome.storage.local.get(null, function(items) {
-	for(var key in items) {
-		if(items.hasOwnProperty(key)) {
-			var item = items[key];
-			if(item.date < minDate) {
-				storageRemoveVideo(key);
+	// Iterate videos
+	chrome.storage.local.get(null, function(items) {
+		for(var key in items) {
+			if(items.hasOwnProperty(key)) {
+				var item = items[key];
+				if(item.date < minDate) {
+					storageRemoveVideo(key);
+				}
 			}
 		}
-	}
+	});
+}
+
+// Run on startup
+chrome.runtime.onStartup.addListener(function() {
+	clearVideos();
 });
